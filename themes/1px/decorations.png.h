a6b060732d36f25891f79a51147e233c17d38ce2
Peter Hofmann <scm@uninformativ.de>
2015-12-28 12:26:30 +0100

    Add exemplary 1px theme without titlebar

---
+++ themes/1px/decorations.png.h
@@ -0,0 +1,22 @@
+static struct DecorationGeometry dgeo = {
+    .top_height = 1,
+    .left_width = 1,
+    .right_width = 1,
+    .bottom_height = 1,
+};
+
+static struct SubImage dec_coords[] = {
+    { .x = 0, .y = 0, .w = 1, .h = 1 },  /* DecTopLeft */
+    { .x = 0, .y = 0, .w = 1, .h = 1 },  /* DecTop */
+    { .x = 0, .y = 0, .w = 1, .h = 1 },  /* DecTopRight */
+
+    { .x = 0, .y = 0, .w = 1, .h = 1 },  /* DecLeft */
+    { .x = 0, .y = 0, .w = 1, .h = 1 },  /* DecRight */
+
+    { .x = 0, .y = 0, .w = 1, .h = 1 },  /* DecBottomLeft */
+    { .x = 0, .y = 0, .w = 1, .h = 1 },  /* DecBottom */
+    { .x = 0, .y = 0, .w = 1, .h = 1 },  /* DecBottomRight */
+};
+
+static char dec_has_title = 0;
+static struct TitleArea dec_title = { 0 };
---
+++ themes/1px/theme_base.h
@@ -0,0 +1,15 @@
+static char *dec_fonts[] = {
+    "fixed",  /* FontTitle */
+};
+
+static char *dec_font_colors[] = {
+    "#000000",  /* DecTintNormal */
+    "#000000",  /* DecTintSelect */
+    "#000000",  /* DecTintUrgent */
+};
+
+static unsigned int dec_tints[] = {
+    0x444444,  /* DecTintNormal */
+    0x00ff00,  /* DecTintSelect */
+    0xff0000,  /* DecTintUrgent */
+};

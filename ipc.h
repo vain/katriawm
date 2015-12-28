f3d43b2371c6208c50b833266e53e48478cd0216
Peter Hofmann <scm@uninformativ.de>
2015-12-28 16:53:33 +0100

    We don't rely on enum order in ipc.h

--- ipc.h
+++ ipc.h
@@ -22,7 +22,7 @@ enum IPCCommand
      *           |
      *           \- $subject, i.e. what is affected by this action
      */
-    IPCClientClose = 0,
+    IPCClientClose,
     IPCClientKill,
     IPCClientMoveList,
     IPCClientMoveMouse,

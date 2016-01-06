0a868d38b5f56b8e6dd254126dc3bc4b00d46a51
Peter Hofmann <scm@uninformativ.de>
2016-01-06 12:42:27 +0100

    Rework theming to be more portable

--- themes/make_theme_h.sh
+++ themes/make_theme_h.sh
@@ -1,17 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-# Ugh, uhm, I guess this only works on GNU.
-# And ImageMagick is required.
+dir=$1
 
-dir=$(readlink -e "$1")
-
-if [[ -z "$dir" ]]
-then
-    echo "Usage: $0 <directory>" >&2
-    exit 1
-fi
-
-# Reads: $source_image
+# Reads:
+#  - $image
+#  - $image_width
+#  - $image_height
 . "$dir"/metadata || exit 1
 
 echo '#ifndef _WM_THEME_H'
@@ -20,34 +14,26 @@ echo
 cat "$dir"/theme_base.h
 echo
 
-source_image_h="$dir"/"$source_image".h
-
-size=$(identify "$dir/$source_image" | awk '{ print $3 }')
-size_w=${size%x*}
-size_h=${size#*x}
-
-cat "$source_image_h"
+cat "$dir"/"$image".h
 echo
-echo "static unsigned int dec_img_w = $size_w;"
-echo "static unsigned int dec_img_h = $size_h;"
+echo "static unsigned int dec_img_w = $image_width;"
+echo "static unsigned int dec_img_h = $image_height;"
 
-# The following code converts any image readable by ImageMagick into an
-# array of uint32_t. This is achieved by first converting it into a raw
-# PPM file ("-compress losless"). In binary, this format is already
-# pretty close to what we want. We convert it into hex using od. Bytes
-# are grouped in packs of three, thus each group will represent one
-# pixel. We prepend a "0x" and then finally neatly fold the array to 72
-# characters in width.
 echo 'static uint32_t dec_img[] = {'
 
-bytes=$(identify -format '%[fx:w*h*3]' "$dir/$source_image")
-convert -quiet -compress lossless -depth 8 "$dir/$source_image" ppm:- |
-tail -c "$bytes" |
-od -vt x1 -w3 -An |
-sed 's/^/0x/; s/ //g' |
-paste -sd, |
-sed 's/,/, /g' |
-fold -sw 72
+# Our input file is expected to be a raw color PPM file with 8 bit
+# depth per channel. We will now convert this into an uint32_t array.
+# First, we create a hexdump using od. tr + grep will transform this
+# into one byte per line. sed will group it in packs of three bytes and
+# prepend a "0x". "s/[^a-zA-Z0-9]//g" is a simple way to remove newlines
+# from the pattern buffer in a portable way. paste + fold will then
+# transform this into a nicely formatted C array with comma separators.
+
+tail -c `echo $image_width \* $image_height \* 3 | bc` "$dir"/"$image" |
+od -vt x1 -An |
+tr ' ' '\n' | grep -v '^$' |
+sed 's/^/0x/; N; N; s/[^a-zA-Z0-9]//g' |
+paste -sd, - | fold -sw 72
 
 echo '};'
 

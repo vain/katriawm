fb50b507154471d76b9e6fe455f268e981240822
onodera-punpun <onodera@openmailbox.org>
2016-03-23 13:12:57 +0100

    Add punpun theme

---
+++ themes/punpun/colorsfonts.h
@@ -0,0 +1,15 @@
+static char *dec_fonts[] = {
+    "Sans:pixelsize=10:style=bold:antialias=false",  /* FontTitle */
+};
+
+static char *dec_font_colors[] = {
+    "#000000",  /* DecTintNormal */
+    "#000000",  /* DecTintSelect */
+    "#000000",  /* DecTintUrgent */
+};
+
+static uint32_t dec_tints[] = {
+    0xFFFFFF,  /* DecTintNormal */
+    0xFFFFFF,  /* DecTintSelect */
+    0xFFFFFF,  /* DecTintUrgent */
+};

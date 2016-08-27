e62f61e729eb7938f1fbd3ea01e2dbbda10bf4d4
Peter Hofmann <scm@uninformativ.de>
2016-08-27 12:43:04 +0200

    Port theme "1px"

--- themes/1px/colorsfonts.h
+++ themes/1px/colorsfonts.h
@@ -7,9 +7,3 @@ static char *dec_font_colors[] = {
     "#000000",  /* DecTintSelect */
     "#000000",  /* DecTintUrgent */
 };
-
-static uint32_t dec_tints[] = {
-    0x444444,  /* DecTintNormal */
-    0x00ff00,  /* DecTintSelect */
-    0xff0000,  /* DecTintUrgent */
-};
